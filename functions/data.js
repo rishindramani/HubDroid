6e42a034f61452fb791e3d802e7f646fe2268508
Rishindra Mani <rishindramani@outlook.com>
2020-05-02 23:56:29 +0530

    Solution Enhancement

--- functions/data.js
+++ functions/data.js
@@ -1,40 +1,51 @@
 'use strict';
 
+// Read and parse data from JSON file.
 const fs = require('fs');
 const rawData = fs.readFileSync('data.json');
 const parsedData = JSON.parse(rawData);
 
+// To sort the items based on price.
 function compare(a, b) {
   if (a.price > b.price) return -1;
   if (b.price > a.price) return 1;
   return 0;
 }
 
-const phones = [];
-const laptops = [];
-const tablets = [];
+// Creating a categories based on the data.
+const lights = [];
+const fans = [];
+const chairs = [];
 
+// Pushing items to array.
 for(let i = 0; i < parsedData.length; i++){
   const device = parsedData[i];
   const deviceData = {
     name: device.Name,
     description: device.Description,
+    rating: device.Rating,
+    sale: device.Sale,
+    img1: device.Image1,
+    category: device.category,
     price: Number(device.Price.replace(/[^0-9.-]+/g,""))
   }
-  if(device.subcategoryLink === 'Laptops' || device.categoryLink === 'Laptops'){
-    laptops.push(deviceData);
+  if(device.category === 'Lights'){
+    lights.push(deviceData);
   }
-  else if(device.subcategoryLink === 'Tablets' ||  device.categoryLink === 'Tablets'){
-    tablets.push(deviceData);
+  else if(device.category === 'Fans'){
+    fans.push(deviceData);
   }
-  else if(device.categoryLink === 'Phones' || device.categoryLink === 'Phones'){
-    phones.push(deviceData);
+  else if(device.category === 'Chairs'){
+    chairs.push(deviceData);
   }
 }
-phones.sort(compare);
-laptops.sort(compare);
-tablets.sort(compare);
 
-exports.phones = phones;
-exports.laptops = laptops;
-exports.tablets = tablets;
+// Sorting the items of array.
+lights.sort(compare);
+fans.sort(compare);
+chairs.sort(compare);
+
+// Exporting the items to be made available in other files.
+exports.lights = lights;
+exports.fans = fans;
+exports.chairs = chairs;
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,33 @@
 "use strict";
 // Import the Dialogflow module from the Actions on Google client library.
-const { dialogflow, Suggestions, List, BasicCard } = require("actions-on-google");
+const {
+  dialogflow,
+  Suggestions,
+  SignIn,
+  BasicCard,
+  Image,
+  Carousel,
+} = require("actions-on-google");
 
-const { phones, laptops, tablets } = require("./data");
+// Import modules for sending emails.
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+const { v4 } = require("uuid");
+
+// Import Data.
+const { lights, chairs, fans } = require("./data");
 
 // Import the firebase-functions package for deployment.
 const functions = require("firebase-functions");
 
 // Instantiate the Dialogflow client.
-const app = dialogflow({ debug: true });
+const app = dialogflow({
+  debug: true,
+  clientId:
+    "577168752299-dkpuh1pbh9p2925jjkbkicee66qb0ofh.apps.googleusercontent.com",
+});
+
+// Global variables to maintain state of the program.
 let price = 0;
 let lastSelected = null;
 let cartItems = false;
@@ -16,189 +35,305 @@ let allCartItems = [];
 
 const welcome = (conv) => {
   conv.ask(
-    `<speak>Hey there! Greetings from Stock Hub! This is BB8.  \nLooking for new Phones, Tablets or Laptops? Well, I am here to assist you!</speak>`
+    `<speak>Hey there! Looking for decorating your beautiful Home? Well, you have come to the right place! We have nice collection of Chairs, Lights, and Fans. What would you like to buy today?</speak>`
   );
   if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
-    const chips = ["Phones", "Laptops", "Tablets"];
+    const chips = ["Lights", "Chairs", "Fans"];
     if (cartItems === true) chips.push("Cart");
     conv.ask(new Suggestions(chips));
   }
 };
+
 // Welcome Intent
 app.intent("Default Welcome Intent", (conv) => welcome(conv));
 
-app.intent("Phones", (conv) => {
-  conv.ask(`<speak> Okay! Let me make things easier for you! Here are some of the best selling smartphones. Select any one for further details. </speak>`);
+// To show different types of lights.
+app.intent("Lights", (conv) => {
 
+  conv.ask(`<speak>Awesome! We have an antique collection of lights which are well suited for every house. Here's the list of our best selling lights. They are just waiting to be placed in your house.</speak>`);
+
+  // Display a horizontal slider.
   conv.ask(
-    new List({
-      title: "Best Selling Smartphones",
-      items: {
-        // Add the first item to the list
-        SELECTION_KEY_P1: {
-          synonyms: [phones[0].name],
-          title: `${phones[0].name} \n $${phones[0].price}`,
-          description: phones[0].description,
-        },
-        // Add the second item to the list
-        SELECTION_KEY_P2: {
-          synonyms: [phones[7].name],
-          title: `${phones[7].name} \n $${phones[7].price}`,
-          description: phones[7].description,
-        },
-        // Add the third item to the list
-        SELECTION_KEY_P3: {
-          synonyms: [phones[5].name],
-          title: `${phones[5].name} \n $${phones[5].price}`,
-          description: phones[5].description,
-        },
-      },
-    })
-  );
-  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
-    conv.ask(new Suggestions(["Laptops", "Tablets"]));
-  }
-});
-
-app.intent("Tablets", (conv) => {
-  conv.ask(`<speak> Here are the best selling tablets. I am sure one of these will match your choice. Select the desired one.</speak>`);
-
-  conv.ask(
-    new List({
-      title: "Best Selling Tablets",
-      items: {
-        // Add the first item to the list
-        SELECTION_KEY_T1: {
-          synonyms: [tablets[0].name],
-          title: `${tablets[0].name} \n $${tablets[0].price}`,
-          description: tablets[0].description,
-        },
-        // Add the second item to the list
-        SELECTION_KEY_T2: {
-          synonyms: [tablets[1].name],
-          title: `${tablets[1].name} \n $${tablets[1].price}`,
-          description: tablets[1].description,
-        },
-        // Add the third item to the list
-        SELECTION_KEY_T3: {
-          synonyms: [tablets[2].name],
-          title: `${tablets[2].name} \n $${tablets[2].price}`,
-          description: tablets[2].description,
-        },
-      },
-    })
-  );
-  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
-    conv.ask(new Suggestions(["Phones", "Laptops"]));
-  }
-});
-
-app.intent("Laptops", (conv) => {
-  conv.ask(`<speak> Cool! Here's the list of best selling laptops. Select your preferred choice. </speak>`);
-
-  conv.ask(
-    new List({
-      title: "Best Selling Laptops",
+    new Carousel({
+      title: "Best Selling Lights",
       items: {
         // Add the first item to the list
         SELECTION_KEY_L1: {
-          synonyms: [laptops[0].name],
-          title: `${laptops[0].name} \n $${laptops[0].price}`,
-          description: laptops[0].description,
+          synonyms: [lights[0].name],
+          title: `${lights[0].name} \n $${lights[0].price}`,
+          image: new Image({
+            url: lights[0].img1,
+            alt: lights[0].name,
+          }),
         },
         // Add the second item to the list
-        SELECTION_KEY_2: {
-          synonyms: [laptops[1].name],
-          title: `${laptops[1].name} \n $${laptops[1].price}`,
-          description: laptops[1].description,
+        SELECTION_KEY_L2: {
+          synonyms: [lights[1].name],
+          title: `${lights[1].name} \n $${lights[1].price}`,
+          image: new Image({
+            url: lights[1].img1,
+            alt: lights[1].name,
+          }),
         },
         // Add the third item to the list
         SELECTION_KEY_L3: {
-          synonyms: [laptops[2].name],
-          title: `${laptops[2].name} \n $${laptops[2].price}`,
-          description: laptops[2].description,
+          synonyms: [lights[2].name],
+          title: `${lights[2].name} \n $${lights[2].price}`,
+          image: new Image({
+            url: lights[2].img1,
+            alt: lights[2].name,
+          }),
+        },
+      },
+    })
+  );
+
+  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
+    conv.ask(new Suggestions(["Fans", "Chairs"]));
+  }
+});
+
+app.intent("Chairs", (conv) => {
+  conv.ask(`<speak> We understand how important comfort is with beautiful build quality. Have a look at our best selling stylish Chairs.</speak>`);
+
+  // Display a horizontal slider.
+  conv.ask(
+    new Carousel({
+      title: "Best Selling Chairs",
+      items: {
+        // Add the first item to the list
+        SELECTION_KEY_C1: {
+          synonyms: [chairs[0].name],
+          title: `${chairs[0].name} \n $${chairs[0].price}`,
+          image: new Image({
+            url: chairs[0].img1,
+            alt: chairs[0].name,
+          }),
+        },
+        // Add the second item to the list
+        SELECTION_KEY_C2: {
+          synonyms: [chairs[1].name],
+          title: `${chairs[1].name} \n $${chairs[1].price}`,
+          image: new Image({
+            url: chairs[1].img1,
+            alt: chairs[1].name,
+          }),
+        },
+        // Add the third item to the list
+        SELECTION_KEY_C3: {
+          synonyms: [chairs[2].name],
+          title: `${chairs[2].name} \n $${chairs[2].price}`,
+          image: new Image({
+            url: chairs[2].img1,
+            alt: chairs[2].name,
+          }),
         },
       },
     })
   );
   if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
-    conv.ask(new Suggestions(["Phones", "Tablets"]));
+    conv.ask(new Suggestions(["Lights", "Fans"]));
   }
 });
 
-app.intent("List - OPTION", (conv, params, option) => {
-  const SELECTED_ITEM_RESPONSES = {
-    SELECTION_KEY_P1: phones[0],
-    SELECTION_KEY_P2: phones[7],
-    SELECTION_KEY_P3: phones[5],
-    SELECTION_KEY_L1: laptops[0],
-    SELECTION_KEY_L2: laptops[1],
-    SELECTION_KEY_L3: laptops[2],
-    SELECTION_KEY_T1: tablets[0],
-    SELECTION_KEY_T2: tablets[1],
-    SELECTION_KEY_T3: tablets[2],
-  };
-  conv.ask("Thank you for selecting the product. Do you wanna add it to cart?");
-  lastSelected = SELECTED_ITEM_RESPONSES[option];
+app.intent("Fans", (conv) => {
+  conv.ask(`<speak> Here are the best selling fans. </speak>`);
+
+  // Display a horizontal slider.
+  conv.ask(
+    new Carousel({
+      title: "Best Selling Fans",
+      items: {
+        // Add the first item to the list
+        SELECTION_KEY_F1: {
+          synonyms: [fans[0].name],
+          title: `${fans[0].name} \n $${fans[0].price}`,
+          image: new Image({
+            url: fans[0].img1,
+            alt: fans[0].name,
+          }),
+        },
+        // Add the second item to the list
+        SELECTION_KEY_F2: {
+          synonyms: [fans[1].name],
+          title: `${fans[1].name} \n $${fans[1].price}`,
+          image: new Image({
+            url: fans[1].img1,
+            alt: fans[1].name,
+          }),
+        },
+        // Add the third item to the list
+        SELECTION_KEY_F3: {
+          synonyms: [fans[2].name],
+          title: `${fans[2].name} \n $${fans[2].price}`,
+          image: new Image({
+            url: fans[2].img1,
+            alt: fans[2].name,
+          }),
+        },
+      },
+    })
+  );
   if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
-    conv.ask(new Suggestions(["Yes", "No"]));
+    conv.ask(new Suggestions(["Chairs", "Lights"]));
   }
 });
 
-app.intent("List - OPTION - no", (conv, params, option) => {
-  conv.ask("No problem. Let me take you to starting again");
-  lastSelected = null;
-  welcome(conv);
+// Handling responses of selected item from sliders.
+app.intent("Carousel - OPTION", (conv, params, option) => {
+  const SELECTED_ITEM_RESPONSES = {
+    SELECTION_KEY_L1: lights[0],
+    SELECTION_KEY_L2: lights[1],
+    SELECTION_KEY_L3: lights[2],
+    SELECTION_KEY_C1: chairs[0],
+    SELECTION_KEY_C2: chairs[1],
+    SELECTION_KEY_C3: chairs[2],
+    SELECTION_KEY_F1: fans[0],
+    SELECTION_KEY_F2: fans[1],
+    SELECTION_KEY_F3: fans[2],
+  };
+
+  // Display specifications of the product.
+  lastSelected = SELECTED_ITEM_RESPONSES[option];
+  let response = '';
+  if (lastSelected.category === 'Chairs') {
+    response = 'Great choice. This chair is very beautiful yet so comfortable. Here are some more details for you to know.'
+  } else {
+    response = `Well your choice is as beautiful as this lamp. It's going to be a new killer combination in your home. Here are some more details.`
+  }
+  conv.ask(response);
+  conv.ask(
+    new BasicCard({
+      title: lastSelected.name,
+      text: lastSelected.description,
+    })
+  );
+  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
+    conv.ask(new Suggestions(["Rating", "Add to cart"]));
+  }
 });
 
-app.intent("List - OPTION - yes", (conv, params, option) => {
-  conv.ask("Great choice!!The Product has been added to the cart!  \nHow can i assist you more?");
+// Add the selected product to the cart.
+app.intent("Add to cart", (conv) => {
+  conv.ask("Product added to the cart! Would you like to check items in your cart or add more products from our collection?");
   price += lastSelected.price;
   allCartItems.push(lastSelected);
   cartItems = true;
   if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
+    conv.ask(new Suggestions(["Cart", "Lights", "Chairs", "Fans"]));
+  }
+});
+
+// Get the rating for the selected product. ( Here we hard coded the rating because we couldn't find the specified API with lots of details.)
+
+app.intent("Rating", (conv) => {
+  conv.ask("Customers seems really happy with this item. We hope you would love it as well.");
+  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
     conv.ask(
-      new Suggestions(["Cart", "Add Phones", "Add Tablets", "Add Laptops"])
+      new BasicCard({
+        title: "Rating: " + lastSelected.rating,
+        text: `
+        5 Star: 2341,
+        4 Star: 331,
+        3 Star: 59,
+        2 Star: 24,
+        1 Star: 12
+        `,
+      })
+    );
+    conv.ask(new Suggestions(["Add to cart"]));
+  }
+});
+
+// Show current items in the cart.
+app.intent("Cart", (conv) => {
+  conv.ask("Here is your new cart details");
+  let cartText = "";
+  let num = 1;
+  allCartItems.forEach((cartItem) => {
+    cartText += `${num++}. ${cartItem.name} $${cartItem.price}\n`;
+  });
+  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
+    conv.ask(
+      new BasicCard({
+        title: "Cart",
+        subtitle: "Cart Total: $" + price,
+        text: cartText,
+      })
     );
   }
+  conv.ask("Proceed to checkout?");
+  conv.ask(new Suggestions(["Checkout", "Lights", "Chairs", "Fans"]));
 });
 
-app.intent("Cart", (conv) => {
-  conv.ask('Here is your updated cart details');
-  let cartText = "";
-  allCartItems.forEach(cartItem => {
-    cartText += `# ${cartItem.name} $${cartItem.price}\n`;
-  })
-  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
-    conv.ask(new BasicCard({
-        title: 'Cart',
-        subtitle: 'Cart Total $' + price,
-        text: cartText
-      }
-    ))
-  }
-  conv.ask('Do you wish to Proceed to checkout?');
-  conv.ask(
-    new Suggestions(["Checkout", "Add Phones", "Add Tablets", "Add Laptops"])
-  );
-});
-
+// Work on checkout
 app.intent("Checkout", (conv) => {
-  conv.ask(
-    "Thanks Andrew for shopping with us. We will ship the products ASAP to your address. Your total is $" +
-    price + ". Order details and tracking ID has been sent to your mobile."
-  );
-  if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
-    conv.ask(new Suggestions(["Exit", "Phones", "Tablets", "Laptops"]));
-  }
-  price = 0;
-  cartItems = false;
-  allCartItems = [];
+  conv.ask(new SignIn("For checkout"));
 });
 
-app.intent("Exit", (conv) => {
-  conv.close(`Thank you for stopping by. See you next time. Have a great day!`);
-
+// If user allowed sign in then order the products in the cart otherwise ask user to sign in to complete the order successfully. For successful orders, email is also sent to user on their email address and order tracking details as well.
+app.intent("Get checkout details", async (conv, params, signin) => {
+  if (signin.status === "OK") {
+    try {
+      const userProfile = conv.user.profile.payload;
+      conv.ask(
+        "Thank you for shopping with us. We will ship the products ASAP to your address. Your total is $" +
+        price +
+        ". Order details and tracking ID has been sent to your email."
+      );
+      conv.ask(
+        new BasicCard({
+          title: "Your order is on its way",
+          text: "Reach out to us at customer.support@lowes.com for any queries",
+          image: new Image({
+            url: 'https://firebasestorage.googleapis.com/v0/b/happy-birthday-74a91.appspot.com/o/truck.PNG?alt=media&token=8c986b99-9ca6-48e7-8368-100a495f5567',
+            alt: 'Delivering your products!'
+          })
+        })
+      );
+      let cartText = "";
+      let num = 1;
+      allCartItems.forEach((cartItem) => {
+        cartText += `${num++}. ${cartItem.name} $${cartItem.price}<br>`;
+      });
+      // Send email to user.
+      const transporter = nodemailer.createTransport(
+        sendgridTransport({
+          auth: {
+            api_key:
+              "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",
+          },
+        })
+      );
+      await transporter.sendMail({
+        to: userProfile.email,
+        from: "rishi06081998@gmail.com",
+        subject: "Thank you for shopping with us",
+        html: `
+          <h2> Cart Total: $'${price} </h2>
+          <hr>
+          <p>
+          ${cartText}
+          </p>
+          <hr>
+          <b> Order ID: ${v4()} </b>
+        `,
+      });
+      if (conv.surface.capabilities.has("actions.capability.SCREEN_OUTPUT")) {
+        conv.ask(new Suggestions(["Exit", "Lights", "Chairs", "Fans"]));
+      }
+      price = 0;
+      cartItems = false;
+      allCartItems = [];
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    conv.ask(
+      `You need to sign in to order items. If you want to sign in, say "Sign me in for checkout"`
+    );
+    conv.ask(new Suggestions(["Checkout", "Lights", "Chairs", "Fans"]));
+  }
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
